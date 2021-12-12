136268cc3023d2c781098a1c8beb03b96e394a25
keitaro-code <92206106+keitaro-code@users.noreply.github.com>
2021-12-12 10:29:42 +0900

    Add files via upload

---
+++ Talk_Theme/js/main.js
@@ -0,0 +1,35 @@
+'use strict'; {
+  const display = document.getElementById('display');
+  const btn = document.getElementById('btn');
+
+  // 質問ボックス
+  const hobbys = ['どんな運動が好き？','1日休みができたら何をする？','最近あった楽しかったことは？','週末は何をよくする？','雨の日は何をしがち？','超晴天の日は何をしたい？','「1人〇〇」何ができる？','どんなゲームが好き？','どんな漫画が好き？','どんな映画をよく見る？','どんな本を読みがち？','どんな系統の服を着てみたい？'];
+  const loves = ['あなたにとって恋とは？','どんな恋愛がしたい？','ファーストキスはいつ？','初恋はいつ？','理想の結婚相手はどんな人？','あなたにとって愛とは？','あなたの好きなタイプは？','初デートどこに行きたい？','結婚したらどんなルール作る？','どんなデートが好き？','インドア派？アウトドア派？','恋愛において何を重視する？','結婚において何を重視する？','一目惚れをしたことある？','好きな人には態度変わる？','「顔か性格」まずどっちから判断する？'];
+  const others = ['今年1の良い買い物は？','10年前に戻るとしたら何をやる？','あなたの夢は？','めちゃうざかった出来事は？','あなたにとって最強のお菓子は？','人生で特に恥ずかしかった出来事は？','生まれ変わるなら何になりたい？','将来住むなら「田舎or都会」？','これからどんな人生を過ごしたい？','あなたにとって友達とは？'];
+
+
+  // ボタンクリック
+  btn.addEventListener('click',() => {
+    if(kinds.value == 'nothing'){
+      display.textContent = 'テーマをお選びください';
+    } else if (kinds.value == 'hobby') {
+      display.textContent = hobbys[Math.floor(Math.random() * hobbys.length)];
+
+    } else if (kinds.value == 'love') {
+      display.textContent = loves[Math.floor(Math.random() * loves.length)]; 
+    } else {
+      display.textContent = others[Math.floor(Math.random() * others.length)]; 
+    }}  );
+
+  const kinds = document.querySelector('select');
+  // const display_h = document.getElementById('display_h');
+  // const display_l = document.getElementById('display_l');
+  // const display_o = document.getElementById('display_o');
+  // const option = document.querySelector('option');
+
+
+
+
+   
+  }
+
